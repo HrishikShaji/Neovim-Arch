59e7ae19cc2ca261629335ea1d030d2ad3de9939
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-03 10:55:36 +0530

    windows ui improved

--- lua/options.lua
+++ lua/options.lua
@@ -1,1 +1,16 @@
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+local _border = "single"
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = _border,
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = _border,
+})
+
+vim.diagnostic.config({
+	float = { border = _border },
+})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,25 +18,33 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
+			local _border = "bordered"
+			require("lspconfig.ui.windows").default_options = {
+				border = _border,
+			}
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+			local on_attach = function(client, bufnr)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { bufnr = bufnr })
+			end
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
+				on_attach = on_attach,
 				capabilities = capabilities,
 			})
 			lspconfig.tsserver.setup({
 				capabilities = capabilities,
-				autostart = true,
+				on_attach = on_attach,
 			})
 			lspconfig.html.setup({
+				on_attach = on_attach,
 				capabilities = capabilities,
 			})
 			lspconfig.tailwindcss.setup({
+				on_attach = on_attach,
 				capabilities = capabilities,
 			})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 }
